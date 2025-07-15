db90565ae67dbe0b07582e78e0eb9436b7a190c2
CrioUser <crio-user@criodo.com>
2025-07-16 01:05:19 +0530

    QKART_QA_MODULE_DEBUGGING_DONE

--- app/src/main/java/QKART_SANITY_LOGIN/Module1/QkartSanity.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Module1/QkartSanity.java
@@ -3,25 +3,13 @@
  */
 package QKART_SANITY_LOGIN.Module1;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class QkartSanity {
 
@@ -119,9 +107,12 @@ public class QkartSanity {
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
+        
         int totalTests = 0;
         int passedTests = 0;
         Boolean status;
+
+        RemoteWebDriver driver = createDriver();
         // Maximize and Implicit Wait for things to initailize
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
