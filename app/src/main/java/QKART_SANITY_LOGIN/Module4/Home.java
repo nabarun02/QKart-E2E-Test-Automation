7390b3b20fb3a0b648e4fe7dd8d9a481806a8028
CrioUser <crio-user@criodo.com>
2025-10-08 03:41:56 +0530

    DONE_QKART_QA_V2_XPATH

--- app/src/main/java/QKART_SANITY_LOGIN/Module4/Home.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Module4/Home.java
@@ -50,10 +50,11 @@ public class Home {
             WebElement searchBox = driver.findElement(By.xpath("//input[@name='search'][1]"));
             searchBox.clear();
             searchBox.sendKeys(product);
+            Thread.sleep(3000);
             // TODO: CRIO_TASK_MODULE_XPATH - M0 Fix broken Xpath
             WebDriverWait wait = new WebDriverWait(driver,30);
-            wait.until(ExpectedConditions.or(ExpectedConditions.textToBePresentInElementLocated(By.className("css-yg30ev6"), product),
-            ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div[3]/div[1]/div[2]/div/h4"))));
+            wait.until(ExpectedConditions.or(ExpectedConditions.textToBePresentInElementLocated(By.className("css-yg30e6"), product),
+            ExpectedConditions.presenceOfElementLocated(By.xpath("//h4[normalize-space(text()) = 'No products found']"))));
             Thread.sleep(3000);
             return true;
         } catch (Exception e) {
